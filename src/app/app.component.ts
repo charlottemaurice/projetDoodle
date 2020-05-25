9e8ea5adb8c18e5abcb327aa3728f578b9af7f91
Charlotte Maurice <charlotte.maurice@toulouse.miage.fr>
2020-05-25 14:44:11 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tp2-web';
+  operande1 = 0 ;
+  operande2 = 0 ;
+  resultat = 0 ;
+  addition() {
+    this.resultat = this.operande1 + this.operande2;
+  }
+  soustraction() {
+    this.resultat = this.operande1 - this.operande2;
+  }
+  multiplication() {
+    this.resultat = this.operande1 * this.operande2;
+  }
+  division() {
+    this.resultat = this.operande1 / this.operande2;
+  }
+}
